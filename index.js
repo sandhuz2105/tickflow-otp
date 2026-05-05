3acaacf0520e3e07d36ee201ae48386d35bc032b
sandhuz2105 <sandhuz2105@gmail.com>
2026-05-05 16:16:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.post('/send-otp', (req, res) => {
     const { phone, otp } = req.body;
     console.log(`Sending OTP ${otp} to ${phone}`);
 
-    const url = `https://2factor.in/API/V1/YOUR_2FACTOR_API_KEY/SMS/${phone}/${otp}/OTP1`;
+    const url = `https://2factor.in/API/V1/Xh9eTOG4SCfFcqR30EWZUYQpuIygwa6ozxs75rtjDmdilb8PAvWbhlmfj5kdOH9esCXqN7V2EMDFr1aQ/SMS/${phone}/${otp}/OTP1`;
     https.get(url, { headers: { 'cache-control': 'no-cache' } }, (apiRes) => {
         let data = '';
         apiRes.on('data', chunk => data += chunk);
